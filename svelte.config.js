bd93e4bd4dc24dc11d248e83b3faf0c5695c130f
cleo <kai@kaidoesthings.xyz>
2025-11-06 18:36:23 -0500

    Switch from deno to vercel (☹️☹️☹️☹️) I'M GONNA MISS YOU DENO for some reason the certificate thingy doesn't work on deploy classic, and deno deploy (the new version) will just like... freeze for a really long time and never actually deploy the build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+
+		})
 	},
 	extensions: ['.svelte', '.svx']
 };
